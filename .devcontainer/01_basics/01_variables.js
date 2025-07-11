881f593419b5d413847b9e2013acc6a81f2267d3
VinayForYou <kumarvinay54718@gmail.com>
2025-07-11 05:20:33 +0000

    changes

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const account_id = 4545
+let account_email = "vinay454@gmail.com"
+var account_passwords = "121211"
+acccountCity = "jaipur"
+
+// account2 = 4544 is not allowed
+
+account_email = "vinay45554@gmail.com"
+account_passwords = "12121441"
+acccountCity = "himachal pradesh"
+
+console.log(account_id);
+console.table([
+  account_id,
+  account_email,
+  account_passwords, ])   
+
+
+// not to use VAR
