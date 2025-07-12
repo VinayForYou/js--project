db3095ba1790c8883255e5a21421d737492238eb
VinayForYou <kumarvinay54718@gmail.com>
2025-07-12 14:49:27 +0000

    coversionOperations

---
+++ .devcontainer/01_basics/03_coversionOperations.js
@@ -0,0 +1,28 @@
+let value = 3
+let negativeValue = -value;
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+
+let str1 = "hello"
+let str2 = "vinay"
+let str3 = str1 + str2;
+console.log(str3); // hellovinay    
+
+ 
+console.log("1" + 2); // 12
+console.log(2 + "1"); // 21         
+console.log(2 + 2 + "1"); // 41
+console.log("1" + 2 + 2); // 122
+
+//tricky conversions
+
+console.log(+true); // true
+console.log( + ""); // 0
+
+
+let gamecounter = 100;
+++gamecounter; // 101
+console.log(gamecounter); // 101
